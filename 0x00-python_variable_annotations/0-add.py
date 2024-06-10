726853f67049c2bebc51d99451035decd3993db3
LWSSIM <wassim.elmnajja2@gmail.com>
2024-06-10 14:28:41 +0100

    added add functions

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""This module contains a function that adds two numbers together (type-annotated)"""
+
+
+def add(a: float, b: float) -> float:
+    """Return sum of 2 floats"""
+    return a + b
